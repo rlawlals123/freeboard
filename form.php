af23e726ae676ea19a6fed5a0faa86ce64298b2a
rlawlals123 <89556996+rlawlals123@users.noreply.github.com>
2022-05-18 17:08:28 +0900

    Add files via upload

---
+++ form.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP+MySQL 입문</title>
+    <link rel="stylesheet" href="style.css">
+    <script>
+        function check_input(){
+            if(!document.member.id.value){
+                alert("아이디를 입력하세요!");
+                document.member.id.focus();
+                return;
+            }
+        if(!document.member.pass.value){
+            alert("비밀번호를 입력하세요!");
+            document.member.pass.focus();
+            return;
+        }
+        if(!document.member.pass_confirm.value){
+            alert("비밀번호확인을 입력하세요!");
+            document.member.pass_confirm.focus();
+            return;
+        }
+        if(!document.member.name.value){
+            alert("이름을 입력하세요!");
+            document.member.name.focus();
+            return;
+        }
+        if (!document.member.email.value){
+            alert("이메일 주소를 입력하세요!");
+            document.member.email.focus();
+            return;
+        }
+        if (document.member.pass.value !=document.member.pass_confirm.value){
+            alert("비밀번호가 일치하지 않습니다.\n다시입력해 주세요!");
+            document.member.pass.focus();
+            document.member.pass.select();
+            return;
+        }
+        document.member.submit();
+        }
+        function check_id(){
+            window.open("check_id.php?id=" + document.member.id.value,
+            "IDcheck",
+            "left=700,top=300,width=380,height=160,scrollbars=no,resizable=yes");
+        }
+    </script>
+</head>
+<body>
+    <form name="member" action="insert.php" method="post">
+        <h2>회원 가입 </h2>
+        <ul class="join_form">
+            <li>
+                <span class="col1">아이디</span>
+                <span class="col2"><input type="text" name="id"></span>
+                <span class="col3"><button type="button" onclick="check_id()">중복체크
+    </button></span>
+    </li>
+    <li>
+    <span class="col1">비밀번호</span>
+    <span class="col2"><input type="password" name="pass"></span>
+    </li>
+    <li>
+    <span class="col1">비밀번호 확인</span>
+    <span class="col2"><input type="password" name="pass_confirm"></span>
+    </li>
+    <li>
+    <span class="col1">이름</span>
+    <span class="col2"><input type="text" name="name"></span>
+    </li>
+    <li>
+    <span class="col1">이메일</span>
+    <span class="col2"><input type="text" name="email"></span>
+    </li>
+    </ul>
+    <ul class="buttons">
+        <li><botton type="button" onclick="check_input()">저장하기
+    </button></li>
+    <li><botton type="button" onclick="reset_form()">취소하기
+    </button></li>
+    </ul>
+    </form>
+            </body>
+</html>
